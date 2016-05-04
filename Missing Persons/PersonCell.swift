881cb189741f9898f88234cf7b4e41384f6c75f6
Sean Perez <seanperez@Seans-MBP.attlocal.net>
2016-05-04 12:29:04 -0700

    Changes from old mac

--- Missing Persons/PersonCell.swift
+++ Missing Persons/PersonCell.swift
@@ -10,4 +10,30 @@ import UIKit
 
 class PersonCell: UICollectionViewCell {
     
+    @IBOutlet weak var personImg: UIImageView!
+    
+    func configureCell(imgUrl: String) {
+        
+        if let url = NSURL(string: imgUrl) {
+            downloadImage(url)
+        }
+    }
+    
+    func downloadImage(url: NSURL) {
+        getDataFromUrl(url) { (data, response, error) in
+            dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                guard let data = data where error == nil else { return }
+                self.personImg.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    func getDataFromUrl(url: NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError?) -> Void)) {
+        
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            
+            completion(data: data, response: response, error: error)
+            
+        }.resume()
+    }
 }
--- Missing Persons/ViewController.swift
+++ Missing Persons/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var selectedImg: UIImageView!
     
-    let baseURL = "http://localHost:6069/img"
+    let baseURL = "http://localHost:6069/img/"
     let missingPeople = ["person1.jpg", "person2.jpg", "person3.jpg", "person4.jpg", "person5.jpg", "person6.png"]
     
     override func viewDidLoad() {
@@ -22,12 +22,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collectionView.delegate = self
         collectionView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func checkForMatch(sender: AnyObject) {
     }
     
@@ -38,6 +38,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PersonCell", forIndexPath: indexPath) as! PersonCell
+        
+        let url = "\(baseURL)\(missingPeople[indexPath.row])"
+        cell.configureCell(url)
         return cell
     }
 
